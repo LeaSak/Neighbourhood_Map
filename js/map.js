5528378abf4e76f89b13b4929261c73f756e59f1
Lea-Ann_Sakmann <leaann.sakmann@gmail.com>
2017-08-02 10:01:57 +0200

    Move creating markers to map.js Add infowindows and marker animation.

--- js/map.js
+++ js/map.js
@@ -1,44 +1,74 @@
 var app = app || {};
 
-(function(){
+(function() {
     'use strict';
 
-    var map = app.map;
-    var bounds = app.bounds;
-
-    app.initMap = function(){
+    app.initMap = function() {
         console.log("initMap");
+        var mapElem = document.getElementById('map');
         //create google map and append it to the page
-        var vienna = {lat: 48.2082, lng: 16.3738};
-        app.bounds = new google.maps.LatLngBounds();
+        var vienna = { lat: 48.2082, lng: 16.3738 };
+        // declare bounds of map
+        var bounds = new google.maps.LatLngBounds();
+        // create one info window for all markers
+        app.vm.infoWindow = new google.maps.InfoWindow({
+            maxWidth: 250
+        });
 
         // Constructor creates a new map - only center and zoom are required.
-        app.map = new google.maps.Map(document.getElementById('map'), {
-          center: vienna,
-          zoom: 13
-          });
+        app.vm.map = new google.maps.Map(mapElem, {
+            center: vienna,
+            zoom: 13
+        });
 
         //create a marker for each location
-        // attach marker to attraction object
-        // app.vm.initialPOIList().forEach(function(attraction, index){
-        // var marker = new google.maps.Marker({
-        //     position: attraction.locations(),
-        //     map: app.map,// this attaches markers to the map
-        //     title: attraction.name(),
-        //     animation: google.maps.Animation.DROP,
-        //     id: index
-        //     });
-        // attraction.marker = marker;
-        // bounds.extend(marker.position); // adjust boundaries for each location
-        // });
+        //attach marker to attraction object
+        app.vm.initialPOIList().forEach(function(attraction, index) {
+            var marker = new google.maps.Marker({
+                position: attraction.locations(),
+                map: app.vm.map,
+                title: attraction.name(),
+                animation: google.maps.Animation.DROP,
+                id: index
+            });
+            attraction.marker = marker;
+            marker.addListener('click', function() {
+                app.getInfoWindowContent(this, app.vm.infoWindow);
+                app.toggleBounce(this);
+            });
+            bounds.extend(marker.position); // adjust boundaries for each location
+        });
 
+        //Extend the boundaries of the map for each marker
+        app.vm.map.fitBounds(bounds);
 
-        // Extend the boundaries of the map for each marker
-        // app.map.fitBounds(bounds);
+        //show info window;
 
-        // apply bindings after initMap
-        var vm = new app.ViewModel();
-        ko.applyBindings(vm);
     };
+
+    // Make markers bounce;
+    // callback function
+    app.toggleBounce = function(marker) {
+        if (marker.getAnimation() !== null) {
+            marker.setAnimation(null);
+        } else {
+            marker.setAnimation(google.maps.Animation.BOUNCE);
+            setTimeout(function() {
+                marker.setAnimation(null)
+            }, 1500);
+        }
+    }
+
+    // Assign marker to infowindow marker property
+    // Add marker specific content to info window
+    // clear marker content on close
+    app.getInfoWindowContent = function(marker, infowindow){
+          infowindow.marker = marker;
+          infowindow.setContent('<div>' + marker.title + '</div>');
+          infowindow.open(app.vm.map, marker);
+          infowindow.addListener('closeclick',function(){
+            infowindow.setMarker = null;
+          });
+  };
 
 })();
--- js/viewmodel.js
+++ js/viewmodel.js
@@ -8,41 +8,34 @@ var app = app || {};
         var Attraction = function(data){
             this.name = ko.observable(data.name);
             this.locations = ko.observable(data.locations);
+            this.marker = "";
         };
 
         /*
          * View Model of App
          */
-        app.ViewModel = function(){
+        var ViewModel = function(){
             console.log("ViewModel");
             var self = this;
             //stores attractions as an array of object instances
             this.initialPOIList = ko.observableArray([]);
 
             // add each object instance to the array
-            app.initialPOI.forEach(function(attraction, index){
+            app.initialPOI.forEach(function(attraction){
                 self.initialPOIList.push(new Attraction(attraction));
             });
 
-            // attach marker
-            self.initialPOIList().forEach(function(attraction, index){
-            var marker = new google.maps.Marker({
-                position: attraction.locations(),
-                map: app.map,// this attaches markers to the map
-                title: attraction.name(),
-                animation: google.maps.Animation.DROP,
-                id: index
-                });
-            attraction.marker = marker;
-            app.bounds.extend(marker.position); // adjust boundaries for each location
-            });
-
-            // Extend the boundaries of the map for each marker
-            app.map.fitBounds(app.bounds);
-
-            // click test
-            this.clickEvent = function(obj){
-                console.log(obj.marker.id);
+            // bounce target when link is clicked
+            this.bounceTarget = function(obj){
+                var pin = obj.marker;
+                app.getInfoWindowContent(pin, app.vm.infoWindow);
+                app.toggleBounce(pin);
             }
-        }
+        };
+
+        // Assign View Model to a variable
+        // apply bindings
+        app.vm = new ViewModel();
+        ko.applyBindings(app.vm);
+
     })();
